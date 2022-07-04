647695db7cfb34be236b5ee169bfd8f1d902685c
Nazzareno Massari <nas@naszam.co>
2022-07-04 13:57:05 +0100

    Cleanups

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 build  :; dapp --use solc:0.6.12 build
 clean  :; dapp clean
-test   :; make && ./test.sh $(match)
+test   :; ./test.sh $(match)
--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@ set -e
 
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
 
-
 if [[ -z "$1" ]]; then
   dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" -v
 else
